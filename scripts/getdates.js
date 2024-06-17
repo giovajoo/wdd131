641879b6d78a66222f7b416998d5db3fbcfb33b1
giovajoo <168290479+giovajoo@users.noreply.github.com>
2024-06-17 11:14:28 -0500

    ok

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,15 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let currentYearElement = document.getElementById('currentYear');
-    
-    
-    let currentYear = new Date().getFullYear();
-    currentYearElement.textContent = currentYear;
 
-   
-    let lastModifiedElement = document.getElementById('lastModifiedDate');
-    
-  
-    let lastModifiedDate = document.lastModified;
-    lastModifiedElement.textContent = lastModifiedDate;
-});
+const currentyear = document.querySelector("#currentyear");
+
+alert(document.lastModified);
+
+
+
+
+
+
+
+
 
