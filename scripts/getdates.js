9f440663759a299daef430387500c23785fe7013
giovajoo <168290479+giovajoo@users.noreply.github.com>
2024-06-17 11:57:26 -0500

    ok

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,7 +4,7 @@ var currentyear = new Date().getFullYear();
 var lastModifiedDate = document.lastModified;
 
 document.getElementById('currentyear').textContent = currentyear;
-document.getElementById('lastModified').textContent = 'lastModified: ' + lastModifiedDate;
+document.getElementById('lastModified').textContent = 'Last Modification: ' + lastModifiedDate;
 
 
 
