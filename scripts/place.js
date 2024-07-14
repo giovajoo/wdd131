813221b0c8b57c4f4cf397b167f8995d4e63a1d5
giovajoo <168290479+giovajoo@users.noreply.github.com>
2024-07-13 23:22:37 -0500

    enter

--- scripts/place.js
+++ scripts/place.js
@@ -1,3 +1,12 @@
+var currentyear = new Date().getFullYear();
+
+var lastModifiedDate = document.lastModified;
+
+document.getElementById('currentyear').textContent = currentyear;
+document.getElementById('lastModified').textContent = 'Last Modification: ' + lastModifiedDate;
+
+
+
 const temperatureCelsius = 18; 
 const windSpeedKmph = 15;
 
